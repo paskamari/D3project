383526aba04bb90de45a385d7d9151866de86103
Hamid Paskamari <hpaskamari@gmail.com>
2013-03-10 16:43:27 +0330

    add script page for javascript code

---
+++ script/script.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+//background image
+
+function squares() {
+	document.getElementById('bodysite').className='timeout squares';
+
+}
+function bgnoise() {
+	document.getElementById('bodysite').className='timeout bgnoise';
+
+}
+function white_carbon() {
+	document.getElementById('bodysite').className='timeout white_carbon';
+
+}
+function lghtmesh() {
+	document.getElementById('bodysite').className='timeout lghtmesh';
+
+}
+function white_carbonfiber() {
+	document.getElementById('bodysite').className='timeout white_carbonfiber';
+
+}
+
+//

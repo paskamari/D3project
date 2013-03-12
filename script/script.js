19d73e3adb26bd933c73aa1373bbd85d41003c67
Hamid Paskamari <hpaskamari@gmail.com>
2013-03-12 16:02:10 +0330

    update

--- script/script.js
+++ script/script.js
@@ -4,19 +4,15 @@
 
 function squares() {
 	document.getElementById('bodysite').className='timeout squares';
-
 }
 function bgnoise() {
 	document.getElementById('bodysite').className='timeout bgnoise';
-
 }
 function white_carbon() {
 	document.getElementById('bodysite').className='timeout white_carbon';
-
 }
 function lghtmesh() {
 	document.getElementById('bodysite').className='timeout lghtmesh';
-
 }
 function white_carbonfiber() {
 	document.getElementById('bodysite').className='timeout white_carbonfiber';
@@ -25,4 +21,16 @@ function white_carbonfiber() {
 //onmouseout menu4
 function mouseout() {
 	document.getElementById('containbox').className='contain';
+}
+
+///ofghjboiehoie
+
+function menu4click(){
+	document.getElementById('containbox').style.animation="ttt 1s forwards";	
+}
+
+
+function reply_click(clicked_id)
+{
+    alert(clicked_id);
 }

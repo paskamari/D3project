0d2f56882efb19ab5cf7e3e885320f5da1bd9035
Hamid Paskamari <hpaskamari@gmail.com>
2013-03-15 15:58:21 +0330

    update

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+	var menu4 = $('.menu4taraf > li');
+	menu4.addClass("btnmenu");
+	menu4.click(function(){
+		$(menu4).removeClass("btnmenuselect");
+		$(this).addClass("btnmenuselect");
+	});
+});
+
 // JavaScript Document
 
 //background image
